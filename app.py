ad33905adb8673af7e07d73226240f2d78761dc7
Seydina Ababacar NDOUR <ndoura675@gmail.com>
2025-02-21 21:16:15 +0000

    Ajout de document.html et des fichiers externes

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
-from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     return render_template('index.html') 
+
+@app.route('/cloud') 
+def cloud():
+    return render_template('cloud.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
